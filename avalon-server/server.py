36a4f8c8d0d261fc5c18919146e4795103599aea
Praveen Batra <praveen.batra@berkeley.edu>
2020-04-17 11:40:01 -0700

    move build folder

--- avalon-server/server.py
+++ avalon-server/server.py
@@ -4,7 +4,7 @@ from functools import wraps
 from flask import Flask
 
 # from https://blog.miguelgrinberg.com/post/how-to-deploy-a-react--flask-project
-app = Flask(__name__, static_folder='../avalon-client/build')
+app = Flask(__name__)
 @app.route('/ignore/<params>')
 def index(params):
     return app.send_static_file('index.html')
---
+++ avalon-server/static/precache-manifest.acd25dd131b7fc0e9cda9fca8a0ec800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991baef37042632cc9abbce6fa43a741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7a01615c1983286e863",
+    "url": "/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "3a1bbc10f9373e559409",
+    "url": "/static/css/main.38bf7a45.chunk.css"
+  },
+  {
+    "revision": "f7a01615c1983286e863",
+    "url": "/static/js/2.746fc6b9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.746fc6b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1bbc10f9373e559409",
+    "url": "/static/js/main.f17e0315.chunk.js"
+  },
+  {
+    "revision": "0fa0690fd0dc51df2223",
+    "url": "/static/js/runtime-main.40218ebd.js"
+  }
+]);
