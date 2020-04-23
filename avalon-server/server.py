f050f5d6bbf8c80a48f40c152211a20314024037
Praveen Batra <praveen.batra@berkeley.edu>
2020-04-22 17:19:47 -0700

    undo

--- avalon-server/server.py
+++ avalon-server/server.py
@@ -83,7 +83,7 @@ game_state = new_game_state()
 
 def sees(i, j):
     a, b = player_roles[i], player_roles[j]
-    if i == j or True:
+    if i == j:
         return player_roles[i] + ('+assassin' if i == assassin else '')
     if a == 'merlin' and alignments[b] == 'bad' and b != 'mordred':
         return 'bad'
